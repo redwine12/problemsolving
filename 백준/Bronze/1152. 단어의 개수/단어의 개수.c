38f8821b2e9050407f00ad9ee0ec1f61786d21f2
jaeyounggg <100763528+jaeyounggg@users.noreply.github.com>
2022-10-10 00:27:43 +0900

    [Bronze II] Title: 단어의 개수, Time: 12 ms, Memory: 1972 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.c
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.c
@@ -1,31 +1,28 @@
+
 #include <stdio.h>
 #include <string.h>
 
-int main(void)
-{
-    int i, len, cnt = 0;
-    char arr[1000000];
-    
-    scanf("%[^\n]", arr);
-    len = strlen(arr);
-    
-    /* 공백 문자 하나만 입력 시 0으로 출력 후 종료 */
-    if(len == 1)
-    {
-    	if(arr[0] == ' ')
-        {
-            printf("0\n");
-    		return 0;
-        }
+int main() {
+  char arr[1000000];
+  scanf("%[^\n]", arr);
+  int count = 0, len;
+
+  len = strlen(arr);
+
+  if (len == 1) {
+    if (arr[0] == ' ')
+      { 
+        printf("0\n");
+        return 0;
+      }
+  }
+
+  for (int i = 1; i < len-1; i++) { 
+    if (arr[i] == ' ') {
+      count++;
     }
-    
-    /* 공백 문자 갯수 인식 */
-    for(i = 1; i < len-1; i++)
-    {
-        if(arr[i] == ' ')
-            cnt++;
-    }
-    
-    printf("%d\n", cnt + 1);
-    return 0;
+  }
+
+  printf("%d", count + 1);
+  return 0;
 }
