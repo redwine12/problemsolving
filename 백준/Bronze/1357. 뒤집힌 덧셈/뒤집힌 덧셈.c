0de9a108b712f0b274c4d9136eb3caa6d374dce9
jaeyounggg <100763528+jaeyounggg@users.noreply.github.com>
2022-10-16 00:20:09 +0900

    [Bronze II] Title: 뒤집힌 덧셈, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/1357. 뒤집힌 덧셈/뒤집힌 덧셈.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int rev(int a)  {
+  int sum = 0;
+  while(a!=0) 
+    {   
+        sum=sum*10;
+        sum = sum + a%10; 
+        a/=10;
+    } 
+  return sum;
+}
+
+int main() {
+  int a,b;
+  scanf("%d %d", &a, &b);
+  printf("%d", rev(rev(a)+rev(b)));
+
+  return 0;
+}
