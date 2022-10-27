b3e3ac97b00694e291d429ba4ebbf9dd3dc9099d
jaeyounggg <migonyoung01@gmail.com>
2022-10-27 18:36:12 +0900

    Initial Commit

---
+++ problemsolving/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  problemsolving
+//
+//  Created by 이재영 on 2022/10/27.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
